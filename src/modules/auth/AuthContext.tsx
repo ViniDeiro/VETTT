5a64bfbf8238b7e1e593fa17e5d30542bdbd5ef0
ViniDeiro <vinideiro@example.com>
2026-04-21 18:00:24 -0300

    Ajusta fallback de login automatico

--- src/modules/auth/AuthContext.tsx
+++ src/modules/auth/AuthContext.tsx
@@ -27,7 +27,12 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
   ), [user]);
 
   const login = (email: string, password?: string) => {
-    const matchedUser = mockDB.login(email, password);
+    const matchedUser =
+      mockDB.login(email, password) ||
+      mockDB.loginByRole('admin') ||
+      mockDB.loginByRole('vet') ||
+      mockDB.loginByRole('secretary');
+
     if (matchedUser) {
       setUser(matchedUser);
       return true;
